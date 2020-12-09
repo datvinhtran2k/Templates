4e52c749a126aa9703af676f64dd9f1640624582
Vinh Dat <datvinhtran.dut@gmail.com>
2020-12-09 09:24:34 +0700

    Templates

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
 //Select element function
-const selectElement = function (element){
-	return document.querySelector(element);
+const selectElement = function (element) {
+  return document.querySelector(element);
 };
 
-let menuToggler = selectElement('.menu-toggle');
-let body = selectElement('body');
+let menuToggler = selectElement(".menu-toggle");
+let body = selectElement("body");
 
-menuToggler.addEventListener('click', function(){
-	body.classList.toggle('open');
+menuToggler.addEventListener("click", function () {
+  body.classList.toggle("open");
 });
 
 // Scroll reveal
 window.sr = ScrollReveal();
 
-sr.reveal('.animate-left', {
-	origin: 'left',
-	duration: 1000,
-	distance: '25rem',
-	delay: 200
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 200,
 });
 
-sr.reveal('.animate-right', {
-	origin: 'right',
-	duration: 1000,
-	distance: '25rem',
-	delay: 400
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 400,
 });
 
-sr.reveal('.animate-top', {
-	origin: 'top',
-	duration: 1000,
-	distance: '25rem',
-	delay: 400
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 400,
 });
 
-sr.reveal('.animate-bottom', {
-	origin: 'bottom',
-	duration: 1000,
-	distance: '25rem',
-	delay: 400
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 400,
 });
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
